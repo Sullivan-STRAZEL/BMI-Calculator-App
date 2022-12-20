e71c5f8e16c911df8151da4d2963d0563b92cb50
Sullivan STRAZEL <strazel.sullivan@hotmail.fr>
2022-12-20 01:47:22 +0100

    feat: Ajout des fonctions Javascript

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const BMIData = [
-  { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
-  { name: "Bonne santé", color: "green", range: [18.5, 25] },
-  { name: "Surpoids", color: "lightcoral", range: [25, 30] },
-  { name: "Obésité modérée", color: "orange", range: [30, 35] },
-  { name: "Obésité sévère", color: "crimson", range: [35, 40] },
-  { name: "Obésité morbide", color: "purple", range: 40 },
-];
+"use strict";
 
-// IMC = poids en kg / taille² en m
+function GetBMIResult() {
+   
+   let sizeInput = document.querySelector("#size-input").value;
+   let weightInput = document.querySelector("#weight-input").value;
+   let calculationResult = document.querySelector(".calculation-result");
+   let calculationText = document.querySelector(".calculation-text");
+
+   let BMIResult = (weightInput / Math.pow(sizeInput/100, 2)).toFixed(1);
+   
+   calculationResult.textContent = BMIResult;
+
+   const BMIData = [
+      { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
+      { name: "Bonne santé", color: "green", range: [18.5, 25] },
+      { name: "Surpoids", color: "lightcoral", range: [25, 30] },
+      { name: "Obésité modérée", color: "orange", range: [30, 35] },
+      { name: "Obésité sévère", color: "crimson", range: [35, 40] },
+      { name: "Obésité morbide", color: "purple", range: 40 }
+   ];
+
+   if (BMIResult <= 0) {
+      calculationText.textContent = BMIData[0].name
+   }
+   else if (BMIResult > BMIData[BMIData.length - 1].range) {
+      calculationText.textContent = BMIData[BMIData.length - 1].name
+   }
+   else {
+      calculationText.textContent = BMIData[BMIData.indexOf(BMIData.find(el => BMIResult > el.range[0] && BMIResult <= el.range[1]))].name
+   }
+
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", GetBMIResult)
